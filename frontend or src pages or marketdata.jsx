2a72cf3b4641847d10eff1b2d445a0e7a5483f3d
parvathi982005 <parvathitutarapu@gmail.com>
2026-06-21 08:27:30 +0530

    frontend or src pages or marketdata.jsx

---
+++ frontend or src pages or marketdata.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function MarketData() {
+  return (
+    <div>
+      <h2>Market Data Module</h2>
+      <p>Rice Price: ₹2500/quintal</p>
+      <p>Maize Price: ₹2200/quintal</p>
+    </div>
+  );
+}
+
+export default MarketData;
