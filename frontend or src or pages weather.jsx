d30ee0a76f28972bfb50559e40cae2e5592cb57f
parvathi982005 <parvathitutarapu@gmail.com>
2026-06-21 08:24:52 +0530

    frontend or src or pages weather.jsx

---
+++ frontend or src or pages weather.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Weather() {
+  return (
+    <div>
+      <h2>Weather Module</h2>
+      <p>Temperature: 30°C</p>
+      <p>Humidity: 70%</p>
+    </div>
+  );
+}
+
+export default Weather;
