3501955b5c945f8198d25029271b7c120e70da7a
parvathi982005 <parvathitutarapu@gmail.com>
2026-06-21 08:22:49 +0530

    frontend or src or pages or crop.jsx

---
+++ frontend or src or pages or crop.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Crop() {
+  return (
+    <div>
+      <h2>Crop Module</h2>
+      <p>Rice</p>
+      <p>Maize</p>
+      <p>Cotton</p>
+    </div>
+  );
+}
+
+export default Crop;
